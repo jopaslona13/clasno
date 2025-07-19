245a9896a2c9896beeb5d9d6ec3c770f97ba18bd
Anton <anton.bahnak@gmail.com>
2025-07-19 11:39:45 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3mbguj!3x!2-ib-trdqzhmfbsl96*$j7pc*yfa8h(w9=9l_+00'
+
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "clasna_nazva",
+]
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+USE_TZ = False
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = 'static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
